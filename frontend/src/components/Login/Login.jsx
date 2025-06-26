45b302e94520411645ccce6ed512cd504e1ed694
KAUSxIK <165253735+KAUSxIK@users.noreply.github.com>
2025-06-26 10:49:37 +0530

    Update Login.jsx

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const res = await fetch('http://localhost:8002/api/auth/login', {
+      const res = await fetch('https://scholarship-finder-backend-9rkn.onrender.com/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
