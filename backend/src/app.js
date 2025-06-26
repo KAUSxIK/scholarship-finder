d8d3b63637e53367864ca77e9f3ea08933e40c83
KAUSxIK <165253735+KAUSxIK@users.noreply.github.com>
2025-06-26 11:12:25 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,7 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 app.use(cors({
-    origin:  process.env.CLIENT_URL ,
+    origin:  process.env.CLIENT_URI ,
     methods:"GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials: true
 }));
